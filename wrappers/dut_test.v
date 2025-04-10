8c9fd3341965d4185e840d30d275987bafcaea39
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-10 20:27:28 +0300

    Update dut_test.v

--- wrappers/dut_test.v
+++ wrappers/dut_test.v
@@ -1,1 +1,17 @@
-
+module xor_test(
+  input a,
+  input b,
+  output y);
+  wire a;
+  wire b;
+  reg y;
+  dut xor_(
+    .a(a),
+    .b(b),
+    .y(y)
+  );
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
